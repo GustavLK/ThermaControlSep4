48415bde0d35d30146bfcc21b2acffc7091d549b
Gussewest01 <354492@via.dk>
2026-05-11 13:45:35 +0200

    Beskrivelse af hvad du lavede

--- server/src/main/java/server/model/AlarmList.java
+++ server/src/main/java/server/model/AlarmList.java
@@ -1,5 +1,31 @@
 package server.model;
 
-public class AlarmList {
+import java.util.ArrayList;
+import java.util.List;
 
+public class AlarmList {
+    private List<Alarm>alarms;
+
+    public AlarmList(){
+        alarms = new ArrayList<>();
+    }
+
+    public void addAlarm(Alarm alarm){
+        alarms.add(alarm);
+    }
+
+    public List<Alarm> getAll() {
+        return alarms;
+    }
+    public List<Alarm> getByClientId(int id) {
+        List<Alarm> result = new ArrayList<>();
+
+        for (Alarm alarm : alarms) {
+            if (alarm.getClientId() == id) {
+                result.add(alarm);
+            }
+        }
+
+        return result;
+    }
 }
--- server/src/main/java/server/model/ThresholdConfiguration.java
+++ server/src/main/java/server/model/ThresholdConfiguration.java
@@ -1,4 +1,40 @@
 package server.model;
 
 public class ThresholdConfiguration {
+    private double waterFlowMin;
+    private double waterFlowMax;
+    private double temperatureMin;
+    private double temperatureMax;
+    private double energyMax;
+    private double COPMin;
+
+    public ThresholdConfiguration(double waterFlowMin, double waterFlowMax,
+                                  double temperatureMin, double temperatureMax,
+                                  double energyMax, double COPMin) {
+        this.waterFlowMin = waterFlowMin;
+        this.waterFlowMax = waterFlowMax;
+        this.temperatureMin = temperatureMin;
+        this.temperatureMax = temperatureMax;
+        this.energyMax = energyMax;
+        this.COPMin = COPMin;
+    }
+
+    public boolean isWaterFlowNormal(double v) {
+        return v >= waterFlowMin && v <= waterFlowMax;
+    }
+
+    public boolean isTemperatureNormal(double v) {
+        return v >= temperatureMin && v <= temperatureMax;
+    }
+
+    public boolean isEnergyNormal(double v) {
+        return v <= energyMax;
+    }
+
+    public boolean isCOPNormal(double v) {
+        return v >= COPMin;
+    }
 }
+
+
+
