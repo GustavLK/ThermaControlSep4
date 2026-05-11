f6eeca22ffcc150e702e291341ab6a94254e604e
GustavLK <354156@viauc.dk>
2026-05-11 12:53:26 +0200

    AlarmDTO, ThresholdConfigDTO

--- shared/src/main/java/shared/dto/ThresholdConfigDTO.java
+++ shared/src/main/java/shared/dto/ThresholdConfigDTO.java
@@ -1,4 +1,34 @@
 package shared.dto;
 
 public class ThresholdConfigDTO {
+    private double waterFlowMin;
+    private double waterFlowMax;
+    private double temperatureMin;
+    private double temperatureMax;
+    private double energyMax;
+    private double COPMin;
+
+    public double getWaterFlowMin() {
+        return waterFlowMin;
+    }
+
+    public double getWaterFlowMax() {
+        return waterFlowMax;
+    }
+
+    public double getTemperatureMin() {
+        return temperatureMin;
+    }
+
+    public double getTemperatureMax() {
+        return temperatureMax;
+    }
+
+    public double getEnergyMax() {
+        return energyMax;
+    }
+
+    public double getCOPMin() {
+        return COPMin;
+    }
 }
