73cd600753e0e54af12f3335472736cafdc95782
GustavLK <354156@viauc.dk>
2026-05-11 12:44:14 +0200

    SensorDataDTO.java

--- shared/src/main/java/shared/dto/SensorDataDTO.java
+++ shared/src/main/java/shared/dto/SensorDataDTO.java
@@ -1,4 +1,40 @@
 package shared.dto;
 
 public class SensorDataDTO {
+    private int clientId;
+    private String timestamp;
+    private double waterFlow;
+    private double energyConsumption;
+    private double temperature;
+    private double COP;
+
+
+    public SensorDataDTO(int clientId, double waterFlow, double temperature, double COP){
+        this.clientId = clientId;
+        this.waterFlow = waterFlow;
+        this.temperature = temperature;
+        this.COP = COP;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+    public double getWaterFlow(){
+        return waterFlow;
+    }
+    public double getEnergyConsumption(){
+        return energyConsumption;
+    }
+    public double getTemperature(){
+        return temperature;
+    }
+    public double getCOP(){
+        return COP;
+    }
+    public boolean isValid() {
+        return waterFlow >= 0
+        && temperature > -273
+        && energyConsumption >= 0
+        && COP > 0;
+    }
 }
