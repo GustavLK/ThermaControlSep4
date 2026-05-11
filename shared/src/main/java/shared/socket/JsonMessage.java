2b9619f08bd58909bc28e27d3864fc215d51b9cb
GustavLK <354156@viauc.dk>
2026-05-11 13:19:22 +0200

    JsonMessage, pom.xml-shared

--- shared/src/main/java/shared/socket/JsonMessage.java
+++ shared/src/main/java/shared/socket/JsonMessage.java
@@ -1,4 +1,27 @@
 package shared.socket;
 
+import com.google.gson.Gson;
+
 public class JsonMessage {
+    private MessageType type;
+    private String payload;
+    private String version;
+    public MessageType getType() {
+        return type;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+    public void setValues(MessageType type, String payload){
+        this.type = type;
+        this.payload = payload;
+    }
+    public String toJson(){
+        return new Gson().toJson(this);
+    }
+    public static JsonMessage fromJson(String raw){
+        return new Gson().fromJson(raw,JsonMessage.class);
+    }
+
 }
