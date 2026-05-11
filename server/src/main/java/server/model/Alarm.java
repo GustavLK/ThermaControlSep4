47c6c6de025cc33f033c52f39a610013465e06e8
Gussewest01 <354492@via.dk>
2026-05-11 12:48:38 +0200

    Alarm done

--- server/src/main/java/server/model/Alarm.java
+++ server/src/main/java/server/model/Alarm.java
@@ -1,4 +1,35 @@
 package server.model;
 
+import java.time.LocalDateTime;
+
 public class Alarm {
+    private AlarmType alarmType;
+    private LocalDateTime timestamp;
+    private AlarmStatus status;
+    private int clientId;
+
+
+    public Alarm(AlarmType alarmType, LocalDateTime timestamp, AlarmStatus status, int clientId) {
+        this.alarmType = alarmType;
+        this.timestamp = timestamp;
+        this.status = status;
+        this.clientId = clientId;
+    }
+
+    public AlarmType getAlarmType() {
+        return alarmType;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public AlarmStatus getStatus() {
+        return status;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
 }
+
