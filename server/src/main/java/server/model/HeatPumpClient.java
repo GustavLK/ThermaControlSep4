de5fc7b4ef01f024a53a84fd182d79acbc7204eb
GustavLK <354156@viauc.dk>
2026-05-11 14:09:49 +0200

    HeatPumpClient, HeatPumpClientList

--- server/src/main/java/server/model/HeatPumpClient.java
+++ server/src/main/java/server/model/HeatPumpClient.java
@@ -1,5 +1,33 @@
 package server.model;
 
 public class HeatPumpClient {
+    private int id;
+    private String clientName;
+    private ConnectionStatus connectionStatus;
+    private ThresholdConfiguration threshold;
+
+    public HeatPumpClient(int id, String name) {
+        this.id = id;
+        this.clientName = name;
+        this.connectionStatus = ConnectionStatus.DISCONNECTED;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return clientName;
+    }
+    public ConnectionStatus getStatus(){
+        return connectionStatus;
+    }
+
+    public ThresholdConfiguration getThreshold() {
+        return threshold;
+    }
+    public void setStatus(ConnectionStatus status){
+        this.connectionStatus = status;
+    }
 
 }
--- server/src/main/java/server/model/HeatPumpClientList.java
+++ server/src/main/java/server/model/HeatPumpClientList.java
@@ -1,4 +1,37 @@
 package server.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HeatPumpClientList {
+    private int lastUsedId = -1;
+    private List<HeatPumpClient> clients = new ArrayList<>();
+
+    public HeatPumpClientList() {
+    }
+
+    public int addClient(String name) {
+        lastUsedId++;
+        HeatPumpClient client = new HeatPumpClient(lastUsedId, name);
+        clients.add(client);
+        return lastUsedId;
+    }
+
+    public void removeClient(int id) {
+        for (HeatPumpClient client : clients) {
+            if (client.getId() == id) {
+                clients.remove(client);
+                break;
+            }
+        }
+    }
+
+    public HeatPumpClient getClientById(int id) {
+        for (HeatPumpClient client : clients)
+            if (client.getId() == id) {
+                return client;
+            }
+        return null;
+    }
 }
+
