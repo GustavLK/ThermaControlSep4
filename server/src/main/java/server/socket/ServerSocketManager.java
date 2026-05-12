32f1615d66d5865290f713f12db4db7119070ac9
GustavLK <354156@viauc.dk>
2026-05-12 11:51:55 +0200

    MainServer, ServerSocketManager

--- server/src/main/java/server/socket/ServerSocketManager.java
+++ server/src/main/java/server/socket/ServerSocketManager.java
@@ -1,4 +1,46 @@
 package server.socket;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerSocketManager {
+    private ServerSocket welcomeSocket;
+    private ServerClientHandlerPool HANDLER_POOL;
+    private int port;
+
+    public ServerSocketManager(int port) throws IOException {
+        this.port = port;
+        this.welcomeSocket = new ServerSocket(port);
+        this.HANDLER_POOL = new ServerClientHandlerPool();
+    }
+    public void run(){
+        startListening();
+    }
+    public void startListening() {
+        while (true) {
+            try {
+                Socket socket = welcomeSocket.accept();
+                spawnClientHandler(socket);
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+    public void broadcast(String message){
+        HANDLER_POOL.broadcast(message,null);
+    }
+    public void stopServer() {
+        try {
+            welcomeSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void spawnClientHandler(Socket socket) throws IOException {
+        ServerClientHandler handler = new ServerClientHandler(socket);
+        HANDLER_POOL.add(handler);
+        new Thread(handler).start();
+    }
 }
--- server/src/main/java/server/utility/MainServer.java
+++ server/src/main/java/server/utility/MainServer.java
@@ -1,6 +1,12 @@
 package server.utility;
 
+import server.socket.ServerSocketManager;
+
+import java.io.IOException;
+
 public class MainServer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ServerSocketManager server = new ServerSocketManager(8080);
+        new Thread(server::run).start();
     }
 }
