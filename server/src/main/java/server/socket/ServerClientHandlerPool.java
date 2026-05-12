9208b6e60e54f44f68c6fe0c8fb27240ffd802a4
GustavLK <354156@viauc.dk>
2026-05-12 11:30:26 +0200

    ServerClientHandlerPool

--- server/src/main/java/server/socket/ServerClientHandlerPool.java
+++ server/src/main/java/server/socket/ServerClientHandlerPool.java
@@ -1,4 +1,22 @@
 package server.socket;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServerClientHandlerPool {
+    private List<ServerClientHandler> handlers;
+
+    public ServerClientHandlerPool(){
+        this.handlers = new ArrayList<ServerClientHandler>();
+    }
+    public void add(ServerClientHandler handler){
+        handlers.add(handler);
+    }
+    public void broadcast(String message, ServerClientHandler source){
+        for (ServerClientHandler handler : handlers) {
+            if (handler != source) {
+                handler.send(message);
+            }
+        }
+    }
 }
