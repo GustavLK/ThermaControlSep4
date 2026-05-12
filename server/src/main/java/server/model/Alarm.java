6eefd7941a77f87ddeca4506375e177fb29adcde
GustavLK <354156@viauc.dk>
2026-05-12 09:43:40 +0200

    -constructor, Alarm

--- server/src/main/java/server/model/Alarm.java
+++ server/src/main/java/server/model/Alarm.java
@@ -18,12 +18,6 @@ public class Alarm {
         this.status = status;
         this.clientId = clientId;
     }
-    public Alarm(AlarmType alarmType, int clientId) {
-        this.alarmType = alarmType;
-        this.clientId = clientId;
-        this.timestamp = LocalDateTime.now();
-        this.status = AlarmStatus.ACTIVE;
-    }
 
     public AlarmType getAlarmType() {
         return alarmType;
